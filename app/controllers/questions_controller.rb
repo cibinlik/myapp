c06bdc90c2579ff8e6c4d9e0e3d3b1e8bed96688
Volkan Ger <cibinlik@gmail.com>
2014-03-31 23:09:39 +0300

    bugfix

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,24 +3,25 @@ class QuestionsController < ApplicationController
 def new
 end
 
-def create
-	@question = Question.new(params[:post])
-	@question.save
-	redirect_to @question
-	#render text: params[:question].inspect
-end
+	def create
+		#@question = Question.new(params[:question])
+		@question.save
+		redirect_to @question
+		#render text: params[:question].inspect
+	end
 
-private
-def question_params
-	params.require(:question).permit(:question)
-end
+	def show
+		@question = Question.find(params[:id])
+	end
 
-def show
-	@question = Question.find(params[:id])
-end
-
-def index
-	@questions = Question.all
-end
+	def index
+		@questions = Question.all
+	end
+	
+	private
+	
+		def question_params
+			params.require(:question).permit(:question)
+		end
 
 end
