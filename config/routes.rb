ef4a6bbcd8e4f02df4dc12cd94f835d067f7fde1
Volkan Ger <cibinlik@gmail.com>
2014-03-31 22:49:22 +0300

    error resolved

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Myapp::Application.routes.draw do
 	resources :questions
 	root "welcome#index"
 	get "welcome/index"
-	questions GET /questions(.:format) posts#index
+	questions GET /questions(.:format) questions#index
   # root to: "welcome#index" root'un uzun haliymiş.
   
   # The priority is based upon order of creation: first created -> highest priority.
